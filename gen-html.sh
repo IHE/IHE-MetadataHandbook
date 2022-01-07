22e11b7087d484600a7cd47d56f1fc4200e477ee
John Moehrke <JohnMoehrke@gmail.com>
2022-01-07 12:06:03 -0600

    publication scripts for hasndbooks/metadata folder

--- gen-html.sh
+++ gen-html.sh
@@ -1,1 +1,1 @@
-pandoc -r gfm --template=https://github.com/IHE/publications/raw/master/ihe_template.html --metadata title="Document Sharing Metadata Handbook" --metadata path-prefix="../../" -w html -o index.html README.md
+pandoc -r gfm --template=https://github.com/IHE/publications/raw/master/ihe_template.html --metadata title="Document Sharing Metadata Handbook" --metadata path-prefix="../../../" -w html -o index.html README.md
